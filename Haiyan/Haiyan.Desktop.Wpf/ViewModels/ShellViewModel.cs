422d7548918aa752931cd69333d1abfc9f27e317
Thomas Björkroth <thomas.bjorkroth@wsp.com>
2023-02-19 16:02:57 +0100

    WIP materiallayers in wpf datagrid

--- Haiyan/Haiyan.Desktop.Wpf/ViewModels/ShellViewModel.cs
+++ Haiyan/Haiyan.Desktop.Wpf/ViewModels/ShellViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Caliburn.Micro;
 using Haiyan.DataCollection.Ifc.ModelReaders;
 using Haiyan.Domain.BuildingElements;
+using Haiyan.Domain.Materials;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Haiyan.Desktop.Wpf.ViewModels
 {
@@ -10,17 +12,39 @@ namespace Haiyan.Desktop.Wpf.ViewModels
         public ShellViewModel()
         {
             BuildingElements = new ObservableCollection<HaiyanBuildingElement>();
+            MaterialLayers = new ObservableCollection<HaiyanMaterialLayer>();
 
             var modelElements = ModelReader.Read("");
             modelElements.ForEach(element => { BuildingElements.Add(element); });
+
+            var layers = modelElements
+                .Select(x => x.Material.Layers)
+                .SelectMany(x => x)
+                .ToList();
+
+            layers.ForEach(layer => { MaterialLayers.Add(layer); });
+
         }
 
         private ObservableCollection<HaiyanBuildingElement> _buildingElements;
-        public ObservableCollection<HaiyanBuildingElement> BuildingElements { get => _buildingElements; 
+        public ObservableCollection<HaiyanBuildingElement> BuildingElements { 
+            get => _buildingElements; 
             set
             {
                 _buildingElements = value;
                 NotifyOfPropertyChange(() => BuildingElements);
+            }
+        }
+
+
+        private ObservableCollection<HaiyanMaterialLayer> _materialLayers;
+        public ObservableCollection<HaiyanMaterialLayer> MaterialLayers
+        {
+            get => _materialLayers;
+            set
+            {
+                _materialLayers = value;
+                NotifyOfPropertyChange(() => MaterialLayers);
             }
         }
     }
